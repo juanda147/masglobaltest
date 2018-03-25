d84ff847ff5456261b8bf1be2cf356d472a0e71b
Juan Rios <juandacastaneda@gmail.com>
2018-03-25 13:12:31 -0500

    Commit project

---
+++ HandsOn/HandsOn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HandsOn.Startup))]
+namespace HandsOn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
